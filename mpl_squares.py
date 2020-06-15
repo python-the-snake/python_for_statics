de8788251ac3efcaea0e1a99f9cccdbaedf15ec1
python-the-snake <dmitriy-shalimov@yandex.ru>
2020-06-15 21:00:35 +0300

    changes in mpl

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,5 +1,14 @@
 import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25, 36]
-plt.plot(squares)
+plt.plot(squares, linewidth=5)
+
+# name of the diagram
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# Fonts
+plt.tick_params(axis='both', labelsize=14)
+
 plt.show()
